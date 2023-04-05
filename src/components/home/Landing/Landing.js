913b71b41653f069b56269e7c732be37ff10137d
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-05 21:23:28 +0530

    Adjusted landing font

--- src/components/home/Landing/Landing.js
+++ src/components/home/Landing/Landing.js
@@ -6,11 +6,11 @@ function Landing() {
         <div className='scroller'>
             <div className="scroll-window">
                 <div className="scroll-container">
-                    <div className="scrolling">IDEAS</div>
-                    <div className="scrolling">WORTH</div>
-                    <div className="scrolling">SPREADING</div>
-                    <div className="scrolling tedx">TEDx</div>
-                    <div className="scrolling">IDEAS</div>
+                    <div className="scrolling text-5xl md:text-9xl">IDEAS</div>
+                    <div className="scrolling text-5xl md:text-9xl">WORTH</div>
+                    <div className="scrolling text-5xl md:text-9xl">SPREADING</div>
+                    <div className="scrolling tedx text-5xl md:text-9xl">TEDx</div>
+                    <div className="scrolling text-5xl md:text-9xl">IDEAS</div>
                 </div>
             </div>
         </div>
