e8f154005edbef30009946cf3d5eff1ad0298678
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-05 21:55:29 +0530

    feat: transition added

--- src/components/home/Landing/Landing.js
+++ src/components/home/Landing/Landing.js
@@ -2,15 +2,38 @@ import React from 'react'
 import './Landing.css'
 
 function Landing() {
+    let index = 0,
+    interval = 1000
+
+    const rand = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const animate = star => {
+            star.style.setProperty('--star-left', `${rand(-10, 100)}%`)
+        star.style.setProperty('--star-top', `${rand(-40, 80)}%`)
+        star.style.animation = "none";
+        star.style.animation = "";
+    }
+
+    for(const star of document.getElementsByClassName('magic-star')){
+    setTimeout(()=>{
+        animate(star)
+        setInterval(()=> {
+        animate(star)
+        }, 1000);    
+    }, index++ * (interval / 3))
+
+    }
     return (
         <div className='scroller'>
             <div className="scroll-window">
                 <div className="scroll-container">
-                    <div className="scrolling text-5xl md:text-9xl">IDEAS</div>
-                    <div className="scrolling text-5xl md:text-9xl">WORTH</div>
-                    <div className="scrolling text-5xl md:text-9xl">SPREADING</div>
-                    <div className="scrolling tedx text-5xl md:text-9xl">TEDx</div>
-                    <div className="scrolling text-5xl md:text-9xl">IDEAS</div>
+                    <div className="scrolling text-5xl md:text-9xl magic-text">IDEAS</div>
+                    <div className="scrolling text-5xl md:text-9xl magic-text">WORTH</div>
+                    <div className="scrolling text-5xl md:text-9xl magic-text">SPREADING</div>
+                    <div className="scrolling text-5xl md:text-9xl magic-text">TEDx</div>
+                    <div className="scrolling text-5xl md:text-9xl magic-text">IDEAS</div>
                 </div>
             </div>
         </div>
