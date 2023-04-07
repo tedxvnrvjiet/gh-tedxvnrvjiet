ca2f0143cc35202859482677a86b305e57222a2c
yathinkumar1 <yathinkumar303@gmail.com>
2023-04-07 16:56:34 +0530

    about us

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 function about() {
   return (
-    <div>About</div>
+    <div>
+      <p className='mx-16 my-8 font-sans font-bold text-4xl'>About <a className='text-red-600'>TED</a></p>
+      <div className='flex space-x-8 mx-16'>
+        <p className='flex-auto'>
+          TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 30 years ago, TED has grown to support its mission with multiple initiatives. The two annual TED Conferences invite the world's leading thinkers and doers to speak for 18 minutes or less. Many of these talks are then made available, free, at TED.com. TED speakers have included Bill Gates, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson, Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Sal Khan and Daniel Kahneman.
+        </p>  
+        <p className='flex-auto'>
+          The annual TED Conference takes place each spring in Vancouver, British Columbia. TED's media initiatives include TED.com, where new TED Talks are posted daily; the Open Translation Project, which provides subtitles and interactive transcripts as well as translations from volunteers worldwide; the educational initiative TED-Ed. TED has established the annual TED Prize, where exceptional individuals with a wish to change the world get help translating their wishes into action; TEDx, which supports individuals or groups in hosting local, self- organized TED-style events around the world, and the TED Fellows program, helping world-changing innovators from around the globe to amplify the impact of their remarkable projects and activities. Follow TED on Twitter at <a href="http://twitter.com/TEDTalks" target="_blank"><u>http://twitter.com/TEDTalks</u></a>, or on Facebook at <a href="http://facebook.com/TED" target="_blank"><u>http://facebook.com/TED</u></a>.
+        </p>  
+      </div>
+      <p className='mx-16 my-8 font-sans font-bold text-4xl'>About <a className='text-red-600'>TED<sup>x</sup></a></p>
+      <p className='mx-16'>
+        In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)
+      </p>
+      <p className='mx-16 my-8 font-sans font-bold text-4xl'>About <a className='text-red-600'>TED<sup>x</sup></a>VNRVJIET</p>
+      <p className='mx-16'>
+        hifhcigrauooaurgugyar
+      </p>
+    </div>
   )
 }
 
