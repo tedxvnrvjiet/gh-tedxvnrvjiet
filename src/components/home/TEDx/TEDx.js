41f3c1d69ce283b23f5025a4707e49a426166c6f
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-09 17:02:01 +0530

    minor changes

--- src/components/home/TEDx/TEDx.js
+++ src/components/home/TEDx/TEDx.js
@@ -6,7 +6,7 @@ function TEDx() {
         <h1 className="text-center text-5xl text-red-600 font-bold mb-10">
             What is TED<sup>x</sup>?
         </h1>
-        <p className='text-white p-4 text-xl pb-20'>
+        <p className='text-white p-4 text-xl pb-40'>
             In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDxVNRVJIET, where x = independently organized TED event. At our TEDxVNRVJIET event, TED Talks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.
             Please visit <a className="text-red-600" href="https://www.ted.com/about/programs-initiatives/tedx-program">TEDx program</a> for further information.
         </p>
