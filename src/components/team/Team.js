7004503fd9e15b67bd9026e252a5c2bee7d85714
yathinkumar1 <96730909+yathinkumar1@users.noreply.github.com>
2023-04-10 01:03:24 +0530

    Update Team.js

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import TeamMember from './TeamMember.js'
 function Team() {
+  const arr=[
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    }
+  ]
   return (
-    <div>Team</div>
+    <div class="ml-32">
+        <div class="flex flex-wrap my-8">
+        {
+          arr.map((ar,index)=><TeamMember key={index} ar={ar}/>)
+        }
+      </div>
+    </div>
+    
+
   )
 }
 
