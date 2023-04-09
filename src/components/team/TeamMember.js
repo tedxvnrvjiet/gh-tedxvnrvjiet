0556ff368c9dd04f7d4eb7bf3d854bf2f2f2152c
yathinkumar1 <96730909+yathinkumar1@users.noreply.github.com>
2023-04-10 01:02:51 +0530

    Create TeamMember.js

---
+++ src/components/team/TeamMember.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Team.css'
+import { FaLinkedin } from "react-icons/fa";
+
+
+function TeamMember(props) {
+  return (
+    <div class="our-team mx-4">
+    <div class="picture">
+      <img class="img-fluid" src={props.ar.url}/>
+    </div>
+    <div class="team-content">
+      <h3 class="name">{props.ar.name}</h3>
+      <h4 class="title">{props.ar.post}</h4>
+    </div>
+    <ul class="social">
+      <li><a href="https://codepen.io/collection/XdWJOQ/" aria-hidden="true"><FaLinkedin/></a></li>
+      <li><a href="https://codepen.io/collection/XdWJOQ/" aria-hidden="true"><FaLinkedin/></a></li>
+    </ul>
+  </div>
+  )
+}
+
+export default TeamMember
