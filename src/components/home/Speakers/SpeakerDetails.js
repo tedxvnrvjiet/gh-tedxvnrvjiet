63b87ba036e8714c5a6bb43ff2de90de5a9732b4
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-09 13:01:55 +0530

    feat: Speakers

--- src/components/home/Speakers/SpeakerDetails.js
+++ src/components/home/Speakers/SpeakerDetails.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
+import Modal from 'react-modal';
+import LinkedInLogo from "../../../assets/logos/LinkedInLogo";
+import InstagramLogo from "../../../assets/logos/InstagramLogo";
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      bottom: 'auto',
+      marginRight: '-40%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 
 export default function SpeakerDetails(props) {
     const [isHovered, setIsHovered] = useState(false);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    const handleHover = () => {
-        setIsHovered(true);
-    };
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
 
-    const handleLeave = () => {
-        setIsHovered(false);
-    };
+    const handleHover = () => setIsHovered(true);
+    const handleLeave = () => setIsHovered(false);
 
     return (
         <div className="m-4 pb-10">
@@ -17,10 +29,26 @@ export default function SpeakerDetails(props) {
                 className="rounded-full hover:bg-red-600 hover:-translate-y-5 duration-300 text-gray-800 font-semibold border border-red-600 border-4 rounded shadow"
                 onMouseEnter={handleHover}
                 onMouseLeave={handleLeave}
+                onClick={openModal}
             >
                 <img className="w-52 rounded-full" src={props.image} alt={props.name} />
             </button>
             { isHovered && <div className="text-white text-center bg-red-600 rounded-xl -mt-2 p-1">{ props.name }</div> }
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                ariaHideApp={false}
+            >
+                <div className="text-center">
+                    <div className="font-bold text-2xl inline mr-2">
+                        { props.name }
+                    </div>
+                    { props?.linkedin && <a className="p-1 align-text-bottom" href={props.linkedin}><LinkedInLogo /></a>}
+                    { props?.instagram && <a className="p-1 align-text-bottom" href={props.instagram}><InstagramLogo /></a>}
+                </div>
+                <p>{props.description}</p>
+            </Modal>
         </div>
     );
 }
--- src/components/home/Speakers/Speakers.js
+++ src/components/home/Speakers/Speakers.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import SpeakerDetails from './SpeakerDetails'
+import SuryaPrakashGajjalla from '../../../assets/SuryaPrakashGajjalla.png'
 
 const SpeakersInfo = [
     {
         name: 'Anveshi Jain',
         image: 'https://media.licdn.com/dms/image/C4D03AQFpgDWhGj4GVg/profile-displayphoto-shrink_400_400/0/1659384665178?e=1686787200&v=beta&t=LtpQjra_gUShCfSSn9H6vAcHulmbVfkXQBOSQDYdfJI',
-        description: '',
+        description: 'Anveshi Jain is an Indian actor and singer born in Khajuraho. She became the most googled actress after her first web release with 30 Million google searches overnight. She is also a singer and a dating coach. She has been a TedEx Speaker and hosted quite a lot of corporate events and shows.',
         instagram: 'https://www.instagram.com/anveshi25/?igshid=YmMyMTA2M2Y%3D',
         linkedin: `https://www.linkedin.com/in/anveshijain/`
     },
     {
         name: 'Sunil Mundra',
         image: 'https://media.licdn.com/dms/image/C4D03AQEcxEfvDZ2tNg/profile-displayphoto-shrink_400_400/0/1653490294934?e=1686182400&v=beta&t=u18SP46H7HWhBKinWOeuGH6OgAVR_w7dbyPcsaQKbUs',
-        description: `TEDxVNRVJIET is excited to announce speaker Mr. Sunil Mundra, who has over 30 years of experience in the IT industry and is the author of the best-selling book "Enterprise Agility: Being Agile in a Changing World." Currently working as a Principal Consultant-Advisory at Thoughtworks, he has a passion for shaping and executing change roadmaps, improving collaboration between business and technology functions, and coaching IT delivery teams on Agile methods. He has spoken at over 30 conferences across 6 continents on Agility-related topics and has international expertise in coaching, consulting organizations on Agile/lean adoption, transformation, governance, change management, client involvement, stakeholder management, and leadership coaching, as well as conducting Agile Maturity Assessments.`,
+        description: `Sunil Mundra is an experienced IT professional and the author of "Enterprise Agility: Being Agile in a Changing World," is set to speak at TEDxVNRVJIET. With over 30 years of experience in the industry, Sunil currently works as a Principal Consultant-Advisory at Thoughtworks and has spoken at numerous conferences on Agility-related topics worldwide. He is passionate about shaping change roadmaps, improving collaboration between business and technology functions, and coaching IT delivery teams on Agile methods.`,
         linkedin: `https://www.linkedin.com/in/sunilmundra/`
     },
     {
         name: 'Rishabh Singh',
         image: 'https://media.licdn.com/dms/image/C5603AQEcMRU4dI9gOQ/profile-displayphoto-shrink_800_800/0/1590042808206?e=1686182400&v=beta&t=F_6UlXTVnMY0Tqj8QgEGIx59OnlNJ5rmKvZX4pH_3Kg',
-        description: `TEDxVNRVJIET is excited to announce speaker Mr. Rishabh Singh, an accomplished LinkedIn profile expert with 70,000 followers. He has a passion for mentoring individuals and organizations on personal and business branding, content strategies, and lead generation. With extensive experience as a brand strategist, delivering talks at top management colleges, and training startups and mid-size organization employees, he has gained a reputation as a dynamic and engaging speaker who inspires audiences to unlock their full potential and establish a strong brand identity. Rishabh has delivered over 250+ talks, including at IIM Lucknow, IIM Jammu, IIM Sambalpur, IIT Bombay, Tata Institute of Social Sciences, and LinkedIn local, sharing his insights and expertise with over 20,000 people. He has helped numerous CXOs and startup founders achieve their objectives and scale their personal branding to thousands of followers with his core strengths in brand strategy, advisory, and content development.`,
+        description: `Rishabh Singh, a renowned LinkedIn profile expert with 70,000 followers, who is passionate about mentoring individuals and organizations on personal and business branding, content strategies, and lead generation. With extensive experience as a brand strategist, Rishabh has delivered over 250+ talks, including at top management colleges and LinkedIn local, sharing his insights and expertise with over 20,000 people. He has helped numerous CXOs and startup founders scale their personal branding to thousands of followers with his core strengths in brand strategy, advisory, and content development.`,
         linkedin: `https://www.linkedin.com/in/rishabh-arun-singh/`
+    },
+    {
+        name: 'Surya Prakash Gajjalla',
+        image: SuryaPrakashGajjalla,
+        description: 'Surya Prakash Gajjalla has over 20 years of experience in Non Destructive Testing and inspection, with involvement in projects for companies such as Larsen & Toubro Limited and Hindustan Petroleum. He founded Advanced Engineering Studies in 2003 to develop qualified NDT professionals and has trained over 4,000 professionals from 18 countries. He is also the Director of Turbo Engineers & Consultants and is actively involved in implementing various schemes for the Department of Technical Education, while also promoting corporate social responsibility.'
     }
 ]
 
