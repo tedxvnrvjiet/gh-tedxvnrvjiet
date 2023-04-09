e3ff94eb59d86eaafef1cff4a19e8223397a7be1
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-09 11:20:57 +0530

    feat: Speaker hover

--- src/components/home/Speakers/SpeakerDetails.js
+++ src/components/home/Speakers/SpeakerDetails.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SpeakerDetails(props) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(true);
+    };
+
+    const handleLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
-        <div className="m-2">
+        <div className="m-4 pb-10">
             <button
-                className="bg-black hover:bg-red-600 text-gray-800 font-semibold py-2 px-4 border border-red-600 rounded shadow"
+                className="rounded-full hover:bg-red-600 hover:-translate-y-5 duration-300 text-gray-800 font-semibold border border-red-600 border-4 rounded shadow"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleLeave}
             >
-                <img style={{borderRadius: "15px 30px"}} className="w-60" src={props.image} alt={props.name} />
-                <h1 className="text-xl text-white">{ props.name }</h1>
+                <img className="w-52 rounded-full" src={props.image} alt={props.name} />
             </button>
+            { isHovered && <div className="text-white text-center bg-red-600 rounded-xl -mt-2 p-1">{ props.name }</div> }
         </div>
     );
 }
--- src/components/home/Speakers/Speakers.js
+++ src/components/home/Speakers/Speakers.js
@@ -3,21 +3,23 @@ import SpeakerDetails from './SpeakerDetails'
 
 const SpeakersInfo = [
     {
+        name: 'Anveshi Jain',
+        image: 'https://media.licdn.com/dms/image/C4D03AQFpgDWhGj4GVg/profile-displayphoto-shrink_400_400/0/1659384665178?e=1686787200&v=beta&t=LtpQjra_gUShCfSSn9H6vAcHulmbVfkXQBOSQDYdfJI',
+        description: '',
+        instagram: 'https://www.instagram.com/anveshi25/?igshid=YmMyMTA2M2Y%3D',
+        linkedin: `https://www.linkedin.com/in/anveshijain/`
+    },
+    {
         name: 'Sunil Mundra',
         image: 'https://media.licdn.com/dms/image/C4D03AQEcxEfvDZ2tNg/profile-displayphoto-shrink_400_400/0/1653490294934?e=1686182400&v=beta&t=u18SP46H7HWhBKinWOeuGH6OgAVR_w7dbyPcsaQKbUs',
-        description: [`TEDxVNRVJIET is proud to introduce, Mr. Sunil Mundra, an accomplished professional with over three decades of experience in the IT industry. Mr. Mundra is also a best-selling author whose book, "Enterprise Agility: Being Agile in a Changing World," received the #1 rating in two categories on Amazon's 'Hot New Releases' list.`,
-            `As a Principal Consultant-Advisory at Thoughtworks, Mr. Mundra is passionate about shaping and executing roadmaps for change, enhancing collaboration between business and technology functions, and coaching IT delivery teams to embrace Agile methods. His expertise has made him a trusted advisor to major enterprises in various industries such as retail, healthcare, finance, and government.`,
-            `With more than 30 speaking engagements on Agility-related topics across six continents, Mr. Mundra has international experience in coaching and consulting organizations on Agile/Lean adoption, transformation, governance, change management, client involvement, stakeholder management, and leadership coaching. He is also adept at conducting Agile Maturity Assessments. We are thrilled to have him as a speaker at TEDxVNRVJIET.`
-        ]
+        description: `TEDxVNRVJIET is excited to announce speaker Mr. Sunil Mundra, who has over 30 years of experience in the IT industry and is the author of the best-selling book "Enterprise Agility: Being Agile in a Changing World." Currently working as a Principal Consultant-Advisory at Thoughtworks, he has a passion for shaping and executing change roadmaps, improving collaboration between business and technology functions, and coaching IT delivery teams on Agile methods. He has spoken at over 30 conferences across 6 continents on Agility-related topics and has international expertise in coaching, consulting organizations on Agile/lean adoption, transformation, governance, change management, client involvement, stakeholder management, and leadership coaching, as well as conducting Agile Maturity Assessments.`,
+        linkedin: `https://www.linkedin.com/in/sunilmundra/`
     },
     {
         name: 'Rishabh Singh',
         image: 'https://media.licdn.com/dms/image/C5603AQEcMRU4dI9gOQ/profile-displayphoto-shrink_800_800/0/1590042808206?e=1686182400&v=beta&t=F_6UlXTVnMY0Tqj8QgEGIx59OnlNJ5rmKvZX4pH_3Kg',
-        description: [`Meet our next featured TEDx speakers, a talented individual with expertise in personal and business branding, content strategies, and lead generation. With over 70,000 followers on LinkedIn, he has become an accomplished profile expert and a mentor to individuals and organizations.`,
-            `Having extensive experience as a brand strategist, he has delivered talks at top management colleges and trained employees of startups and mid-sized organizations. As a TEDx Co-Organizer and Harvard delegate, he has gained a reputation as a dynamic and engaging speaker who inspires audiences to unlock their full potential and establish a strong brand identity.`,
-            `He is one of the youngest speakers to deliver more than 250 talks, including prestigious institutions like IIM Lucknow, IIM Jammu, IIM Sambalpur, IIT Bombay, and Tata Institute of Social Sciences, as well as LinkedIn Local events. He has shared his insights and expertise with over 20,000 people, helping them achieve their business goals and establish a powerful brand presence on LinkedIn.`,
-            `With his core strengths in brand strategy, advisory, and content development, he has helped numerous CXOs and startup founders achieve their objectives and scale their personal branding to thousands of followers. Mr. Rishabh is one of the finest in the industry and has proven to be pivotal in the lives of those who aim to simplify the complexities of personal and business branding.`
-        ]
+        description: `TEDxVNRVJIET is excited to announce speaker Mr. Rishabh Singh, an accomplished LinkedIn profile expert with 70,000 followers. He has a passion for mentoring individuals and organizations on personal and business branding, content strategies, and lead generation. With extensive experience as a brand strategist, delivering talks at top management colleges, and training startups and mid-size organization employees, he has gained a reputation as a dynamic and engaging speaker who inspires audiences to unlock their full potential and establish a strong brand identity. Rishabh has delivered over 250+ talks, including at IIM Lucknow, IIM Jammu, IIM Sambalpur, IIT Bombay, Tata Institute of Social Sciences, and LinkedIn local, sharing his insights and expertise with over 20,000 people. He has helped numerous CXOs and startup founders achieve their objectives and scale their personal branding to thousands of followers with his core strengths in brand strategy, advisory, and content development.`,
+        linkedin: `https://www.linkedin.com/in/rishabh-arun-singh/`
     }
 ]
 
@@ -28,7 +30,7 @@ function Speakers() {
             <div className="flex flex-wrap justify-center p-4">
                 {
                     SpeakersInfo.map((item)=>
-                        <SpeakerDetails key={item.image} name={item.name} image={item.image} description={item.description} />
+                        <SpeakerDetails key={item.image} name={item.name} image={item.image} description={item.description} instagram={item?.instagram} linkedin={item?.linkedin} />
                     )
                 }
             </div>
