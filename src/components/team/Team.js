12cd9069ed1835cb9aacd8a89bf6e37dacccc3a6
yathinkumar1 <yathinkumar303@gmail.com>
2023-04-10 09:23:28 +0530

    categories

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,7 +1,71 @@
 import React from 'react'
 import TeamMember from './TeamMember.js'
 function Team() {
-  const arr=[
+  const arr1=[
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    }
+  ]
+  const arr2=[
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    },
+    {
+      url:"https://picsum.photos/130/130?image=1027",
+      name:"Michele Miller",
+      post:"Web Developer"
+    }
+  ]
+  const arr3=[
     {
       url:"https://picsum.photos/130/130?image=1027",
       name:"Michele Miller",
@@ -35,9 +99,22 @@ function Team() {
   ]
   return (
     <div class="ml-32">
+      <p className='mx-8 my-8 font-sans font-bold text-4xl '>Cat-1</p>
         <div class="flex flex-wrap my-8">
         {
-          arr.map((ar,index)=><TeamMember key={index} ar={ar}/>)
+          arr1.map((ar,index)=><TeamMember key={index} ar={ar}/>)
+        }
+      </div>
+      <p className='mx-16 my-8 font-sans font-bold text-4xl'>Cat-2</p>
+        <div class="flex flex-wrap my-8">
+        {
+          arr2.map((ar,index)=><TeamMember key={index} ar={ar}/>)
+        }
+      </div>
+      <p className='mx-16 my-8 font-sans font-bold text-4xl'>Cat-3</p>
+        <div class="flex flex-wrap my-8">
+        {
+          arr3.map((ar,index)=><TeamMember key={index} ar={ar}/>)
         }
       </div>
     </div>
