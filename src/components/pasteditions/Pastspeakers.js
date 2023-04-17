d0ec61b942861941dde764d983fbf516ecc2bebf
deepesh_16 <ks.deepesh16@gmail.com>
2023-04-18 02:37:55 +0530

    past edition cards

--- src/components/pasteditions/Pastspeakers.js
+++ src/components/pasteditions/Pastspeakers.js
@@ -1,14 +1,14 @@
 import React ,{useState} from 'react'
 import Videobutton from './Videobutton';
 import cvanand from '../images/year2018/cvanand.jpg'
-import akanshabumb from '../images/year2018/akanshabumb.png'
+import akanshabumb from '../images/year2018/akanshabumb.jpg'
 import rameshkumar from '../images/year2018/rameshkumar.png'
-import arshadquadri from '../images/year2018/arshadquadri.png'
-import jagadeeshkumar from '../images/year2018/jagadeeshkumar.png'
-import rakeshkumar from '../images/year2018/rakeshkumar.png'
+import arshadquadri from '../images/year2018/arshadquadri.jpg'
+import jagadeeshkumar from '../images/year2018/jagadeeshkumar.jpg'
+import rakeshkumar from '../images/year2018/rakeshkumar.jpg'
 import akhilagopalakrishnan from '../images/year2017/akhilagopalakrishnan.png'
-import anuradhanaik from '../images/year2017/anuradhanaik.png'
-import carolyntheresa from '../images/year2017/carolyntheresa.png'
+import anuradhanaik from '../images/year2017/anuradhanaik.jpg'
+import carolyntheresa from '../images/year2017/carolyntheresa.jpg'
 import jawwadpatel from '../images/year2017/jawwadpatel.png'
 import mohanakrishna from '../images/year2017/mohanakrishna.png'
 import prashantdhawan from '../images/year2017/prashantdhawan.png'
@@ -19,14 +19,14 @@ import sureshreddy from '../images/year2017/sureshreddy.jpg'
 import tisramband from '../images/year2017/tisramband.jpg'
 import bandrooh from "../images/year2016/bandrooh.png"
 import gandhikpc from "../images/year2016/gandhikpc.png"
-import nageswararao from "../images/year2016/nageswararao.png"
+import nageswararao from "../images/year2016/nageswararao.jpg"
 import paruvuharimohan from "../images/year2016/paruvuharimohan.png"
 import prasadkantamaneni from "../images/year2016/prasadkantamaneni.png"
-import ramanagogula from "../images/year2016/ramanagogula.png"
+import ramanagogula from "../images/year2016/ramanagogula.jpg"
 import rameshloganthan from "../images/year2016/rameshloganthan.png"
-import sandhyakode from "../images/year2016/sandhyakode.png"
+import sandhyakode from "../images/year2016/sandhyakode.jpg"
 import sanjayenishetty from "../images/year2016/sanjayenishetty.png"
-import seetamurthy from "../images/year2016/seetamurthy.png"
+import seetamurthy from "../images/year2016/seetamurthy.jpg"
 import theodorejonnalagadda from "../images/year2016/theodorejonnalagadda.png"
 import voicebox from "../images/year2016/voicebox.png"
 
@@ -66,7 +66,7 @@ function Pastspeakers({props}) {
                     "image":arshadquadri,
                 },
                 {
-                    "name":"Rakesh Kumar Renukuntala",
+                    "name":"Rakesh Kumar R",
                     "designation":"Entrepreneur, VFX" ,
                     "video":"",
                     "image":rakeshkumar,
@@ -149,18 +149,6 @@ function Pastspeakers({props}) {
             "year":2016,
             "speakers":[
                 {
-                    "name":"Band Rooh",
-                    "designation":"Fusion Band",
-                    "video":"https://youtube.com/embed/yxo7DYT-9C4",
-                    "image":bandrooh,
-                },
-                {
-                    "name":"Gandhi KPC",
-                    "designation":"Forensic Scientist",
-                    "video":"https://youtube.com/embed/QJLPla3cQPo",
-                    "image":gandhikpc,
-                },
-                {
                     "name":"Nageshwara Rao Dukkipati",
                     "designation":"Innovator",
                     "video":"https://youtube.com/embed/crZrM2_wbOY",
@@ -185,6 +173,18 @@ function Pastspeakers({props}) {
                     "image":ramanagogula,
                 },
                 {
+                    "name":"Seeta Murty",
+                    "designation":"School Principal,Author",
+                    "video":"https://youtube.com/embed/JggZuQR3-2U",
+                    "image":seetamurthy,
+                },
+                {
+                    "name":"Band Rooh",
+                    "designation":"Fusion Band",
+                    "video":"https://youtube.com/embed/yxo7DYT-9C4",
+                    "image":bandrooh,
+                },
+                {
                     "name":"Ramesh Loganathan",
                     "designation":"Vice president at Progress software",
                     "video":"https://youtube.com/embed/ncTY1hUBhuA",
@@ -202,11 +202,12 @@ function Pastspeakers({props}) {
                     "video":"https://youtube.com/embed/qIhcXTzQGJQ",
                     "image":sanjayenishetty,
                 },
+                
                 {
-                    "name":"Seeta Murty",
-                    "designation":"School Principal,Author",
-                    "video":"https://youtube.com/embed/JggZuQR3-2U",
-                    "image":seetamurthy,
+                    "name":"Gandhi KPC",
+                    "designation":"Forensic Scientist",
+                    "video":"https://youtube.com/embed/QJLPla3cQPo",
+                    "image":gandhikpc,
                 },
                 {
                     "name":"Theodore Jonnalagadda",
@@ -229,14 +230,14 @@ function Pastspeakers({props}) {
    
   return (
     <div className=''>
-        <h1 className='mobile:text-2xl  md:text-3xl lg:text-4xl mb-8 text-red-700 font-semibold mt-6'>Speakers</h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  lg:gap-xl-12 mobile:ml-8 lg:ml-0">
+        <h1 className='mobile:text-2xl  md:text-3xl lg:text-4xl mb-4 text-red-700 font-semibold mt-6  '>Speakers</h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  lg:gap-xl-12">
             {obj.map((s)=>(
-            <div className="mb-12 pt-6 pb-6 duration-200 rounded-xl  hover:bg-zinc-900 transition ease-in-out delay-150 hover:scale-110 p-6" style={{width:"250px"}}>
-                <img src={s['image']} className="rounded-lg shadow-lg mb-4 mx-auto" alt="" style={{width:"150px",height:"140px"} }/>
+            <div className=" rounded-xl mb-4 duration-200   hover:bg-zinc-900 transition ease-in-out delay-150 hover:scale-105 p-6 mobile:w-68 lg:w-80">
+                <img src={s['image']} className="rounded-lg shadow-lg mb-4 mx-auto" alt="" style={{width:"195px",height:"180px"} }/>
         
-                <p className="font-semibold mb-3 text-red-600 mx-auto text-center text-lg h-10">{s['name']}</p>
-                <p className="text-gray-400 mx-auto text-center h-20">{s['designation']}</p>
+                <p className="font-semibold mb-3 text-red-600 mx-auto text-center text-lg h-6">{s['name']}</p>
+                <p className="text-gray-400 mx-auto text-center h-14">{s['designation']}</p>
                 {s.video!="" && <Videobutton props={s.video}/>}
             </div>
             ))}
