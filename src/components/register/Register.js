1e347d18842c5ac64c4024622fcad1cd3f523250
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-17 23:58:17 +0530

    minor

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -4,7 +4,6 @@ import './Register.css'
 function Register() {
   return (
     <div className='register'>
-        <h1>Register</h1>
           {/* <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link> */}
           <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link>
     </div>
