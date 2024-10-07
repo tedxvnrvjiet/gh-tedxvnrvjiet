561bb16252028013d07179f2b37e2d54532cd469
Gangavaram Sreevibha <123618407+sgangavaram@users.noreply.github.com>
2024-10-07 21:51:10 +0530

    Added coming soon

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import Landing from './Landing/Landing'
-import Timer from './Timer/Timer'
-import TEDx from './TEDx/TEDx'
-import Theme from './Theme/Theme'
-import Speakers from './Speakers/Speakers'
-import Fade from 'react-reveal/Fade';
-import Timeline from './Timeline/Timeline'
+import React from 'react';
+// import Landing from './Landing/Landing';
+// import Timer from './Timer/Timer';
+// import TEDx from './TEDx/TEDx';
+// import Theme from './Theme/Theme';
+// import Speakers from './Speakers/Speakers';
+// import Fade from 'react-reveal/Fade';
+// import Timeline from './Timeline/Timeline';
+import comingSoonImage from './Coming-Soon/COMINGSOON.png'; // Replace with the actual path
+
 function Home() {
   return (
     <div>
-      <Landing />
-      <Timeline/>
-      <Fade right>
-        <TEDx />
-      </Fade>
-      <Fade left>
-        <Theme />
-      </Fade>
-      <Fade right>
-        <Speakers />
-      </Fade>
+      <div style={{ textAlign: 'center'}}> {}
+        <img src={comingSoonImage} alt="Coming Soon" style={{ maxWidth: '100%' }} />
+      </div>
+      {/* <Landing /> */}
+      {/* <Timeline /> */}
+      {/* <Fade right> */}
+        {/* <TEDx /> */}
+      {/* </Fade> */}
+      {/* <Fade left> */}
+        {/* <Theme /> */}
+      {/* </Fade> */}
+      {/* <Fade right> */}
+        {/* <Speakers /> */}
+      {/* </Fade> */}
     </div>
-  )
+    
+  );
 }
 
-export default Home
+export default Home;
