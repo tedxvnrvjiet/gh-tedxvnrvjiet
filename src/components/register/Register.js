574d81bc7e847faa819735e2330052cb9db3ec2e
GopiHarishitha <147143957+GopiHarishitha@users.noreply.github.com>
2024-10-10 22:05:07 +0530

    Added coming-soon image to tickets page (#28)

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import './Register.css'
+import React from "react";
+import "./Register.css";
+import comingSoonImage from "./Coming-Soon/COMINGSOON.png";
 
 function Register() {
   return (
-    <div className='register'>
-          {/* <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link> */}
-          <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link>
+    <div className="register">
+      {/* <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link> */}
+      {/* <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/tedxvnrvjiet/booking" frameborder="0" height="600" width="80%"></iframe><link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" ></link> */}
+      <div
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        {}
+        <img
+          src={comingSoonImage}
+          alt="Coming Soon"
+          style={{
+            width: "100vw",
+            height: "100vh",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
