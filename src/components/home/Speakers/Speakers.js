615ef24cfb86f0e7df9c0f08f4c3b1fbd6f82657
U Harshitha <harshitha.srikk@gmail.com>
2024-10-18 10:08:28 +0530

    Added new speakers to home and tickets page

--- src/components/home/Speakers/Speakers.js
+++ src/components/home/Speakers/Speakers.js
@@ -3,7 +3,10 @@ import SpeakerDetails from './SpeakerDetails'
 import Vanita from '../../../assets/speakers24/Vanita.png'
 import Reddy from '../../../assets/speakers24/Reddy.jpg'
 import Shrenik from '../../../assets/speakers24/Shrenik.JPG'
-
+import Neehar from '../../../assets/speakers24/Neehar.png'
+import Noorul from '../../../assets/speakers24/Noorul.png'
+import Jatin from '../../../assets/speakers24/Jatin.jpg'
+import Hidden from'../../../assets/speakers24/Hidden.png'
 const SpeakersInfo = [
         
     {
@@ -25,6 +28,41 @@ const SpeakersInfo = [
         designation: 'Patient Ambassador, Indian Cancer Society, Global Ca-Larynx Speaker, Cancer survivor',
         image: Shrenik,
         description: ' Mr. Shrenik Shah, Patient Ambassador for the Indian Cancer Society, who will share his inspiring journey as a cancer survivor and motivational speaker. After a 26-year battle with cancer, he transformed his struggles into a powerful message of hope, delivering over 8,000 hours of talks that have touched more than 100,000 lives worldwide.',
+        instagram: ''
+    },
+    {
+        name: 'Mr. Neehar Bisabathini',
+        designation: 'Founder of Churrolto',
+        image: Neehar,
+        description: "At just 19 years old, driven by his two passions, food and technology, Neehar started a brand that has transformed dessert culture with its focus on quality and innovation. He not only built a thriving business but also fostered a strong culture at Churrolto that drives its success. His belief? Even if he's not there, Churrolto will thrive! His goal? To feed a billion!!  Beyond food, Neehar’s passion for technology led him to start Zasa Media Pvt. Ltd., combining creativity and tech expertise to create impactful marketing solutions. His journey, from an idea on the streets to building multimillion-dollar brands, shows how ambition and resilience lead to success.From a dream to a legacy, his story shows true success is building something that thrives beyond you.",
+        instagram: ''
+    },
+    {
+        name: 'Ms. Noorul Zain',
+        designation: "Telangana's first UNICEF Youth Delegate",
+        image: Noorul,
+        description: "Ms. Noorul Zain, a trailblazer in mental health advocacy and healthcare innovation! As Telangana's first UNICEF Youth Delegate, she has represented India on global platforms like the G20 Summit and the United Nations. Noorul has significantly influenced national policies related to mental health and education while advocating for children's rights and educational equity. At just 19, her journey includes being a G20 Delegate, contributing to the National Curriculum Framework, and winning the 'Champions of Change' Award at the Girl Up India Leadership Summit.",
+        instagram: ''
+    },
+    {
+        name: 'Mr. Jatin Wahane',
+        designation: 'Young Rocket Scientist',
+        image: Jatin,
+        description: "Mr. Jatin Wahane, a young rocket scientist who has been honoured with the Karmaveer Chakra and Global Fellowship for his groundbreaking contributions to the space industry. As the Head of Investment and Lead - Tech and Business at Abyom SpaceTech, Jatin is driving transformative innovation and strategic investment, playing a crucial role in shaping the future of space exploration.In his talk, Jatin will share his insights on the powerful connection between technology, investment, and the limitless possibilities of space From science to strategy, his journey shows that innovation knows no boundaries.",
+        instagram: ''
+    },
+    {
+        name: 'Mystery Guest',
+        designation: 'Guess the awesome mystery guest',
+        image: Hidden,
+        description: "",
+        instagram: ''
+    },
+    {
+        name: 'Mystery Guest',
+        designation: 'Guess the awesome mystery guess',
+        image: Hidden,
+        description: "",
         instagram: ''
     }
     
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,73 +1,66 @@
 import React from "react";
-// import "./Register.css";
 import QR_2024 from "../../assets/tickets/QR_2024.jpeg";
-import Vanita from "../../assets/speakers24/Vanita.png"
-import Reddy from "../../assets/speakers24/Reddy.jpg"
-import Shrenik from '../../assets/speakers24/Shrenik.JPG'
+import Vanita from "../../assets/speakers24/Vanita.png";
+import Reddy from "../../assets/speakers24/Reddy.jpg";
+import Shrenik from '../../assets/speakers24/Shrenik.JPG';
+import Neehar from '../../assets/speakers24/Neehar.png';
+import Noorul from '../../assets/speakers24/Noorul.png';
+import Jatin from '../../assets/speakers24/Jatin.jpg';
+import Hidden from '../../assets/speakers24/Hidden.png';
 
 function Register() {
-  // Speakers array
   const speakers = [
-    {
-      name: 'Dr. BVR Mohan Reddy',
-      designation: 'Founder of Cyient and Former Chairperson of NASSCOM',
-      image: Reddy,
-  },
-  {
-      name: 'Ms Vanitha Datla',
-      designation: 'Vice Chairperson and Managing Director of Elico Ltd',
-      image: Vanita,
-  },
-  {
-    name: 'Mr Shrenik Shah',
-    designation: 'Patient Ambassador, Indian Cancer Society, Global Ca-Larynx Speaker, Cancer survivor',
-    image: Shrenik,
-  }
+    { name: 'Dr. BVR Mohan Reddy', designation: 'Founder of Cyient and Former Chairperson of NASSCOM', image: Reddy },
+    { name: 'Ms Vanitha Datla', designation: 'Vice Chairperson and Managing Director of Elico Ltd', image: Vanita },
+    { name: 'Mr Shrenik Shah', designation: 'Patient Ambassador, Indian Cancer Society, Global Ca-Larynx Speaker, Cancer survivor', image: Shrenik },
+    { name: 'Mr. Neehar Bisabathini', designation: 'Founder of Churrolto', image: Neehar },
+    { name: 'Ms. Noorul Zain', designation: "Telangana's first UNICEF Youth Delegate", image: Noorul },
+    { name: 'Mr. Jatin Wahane', designation: 'Young Rocket Scientist', image: Jatin },
+    { name: 'Mystery Guest', designation: 'Hidden', image: Hidden },
+    { name: 'Mystery Guest', designation: 'Hidden', image: Hidden }
   ];
 
   return (
-    <div className="max-w-7xl mx-auto h-screen flex justify-center items-center p-4 max-lg:mt-20">
-      <div className="flex flex-col lg:flex-row w-full h-full">
-        {/* Left side for QR code (60-70% width on larger screens) */}
-        <div className="flex flex-col justify-center items-center lg:w-2/3 gap-5 w-full mb-6 lg:mb-0">
-          <h3 className="text-3xl font-bold text-gray-100">
-            <span className="text-red-500">Scan</span> here to register
-            </h3>
-          <img src={QR_2024} alt="QR SCANNER" className="w-64 rounded-md" />
-          <p className="text-white text-base">or</p>
-          <div className="">
-            <a className="bg-red-500 text-white px-4 py-2 rounded-md" href="https://rzp.io/rzp/d8EBdzG">
-              Click here to pay
-            </a>
-          </div>
+    <div className="max-w-7xl mx-auto h-full flex flex-col items-center lg:flex-row p-4 max-lg:mt-20">
+      {/* Left side for QR code */}
+      <div className="flex flex-col justify-center items-center lg:w-2/3 gap-5 w-full mb-6 lg:mb-0">
+        <h3 className="text-3xl font-bold text-gray-100">
+          <span className="text-red-500">Scan</span> here to register
+        </h3>
+        <img src={QR_2024} alt="QR SCANNER" className="w-64 rounded-md" />
+        <p className="text-white text-base">or</p>
+        <div className="mb-6">
+          <a className="bg-red-500 text-white px-4 py-2 rounded-md" href="https://rzp.io/rzp/d8EBdzG">
+            Click here to pay
+          </a>
         </div>
+      </div>
 
-        {/* Divider */}
-        <div className="flex items-center justify-center lg:w-1/3 w-full gap-8">
-          <div className="hidden lg:block bg-gray-500 h-[300px] w-1 rounded-md"></div>
+      {/* Divider for larger screens */}
+      <div className="hidden lg:flex items-center justify-center lg:w-1/5 w-full gap-8">
+        <div className="hidden lg:block bg-gray-500 h-[300px] w-1 rounded-md"></div>
+      </div>
 
-          {/* Right side for speakers (30% width on larger screens) */}
-          <div className="flex flex-col justify-center items-center">
-            <h3 className="text-3xl font-bold text-gray-100">
-              Our <span className="text-red-500">Speakers</span>
-            </h3>
-            <div className="grid grid-cols-2 gap-6 mt-6">
-              {speakers.map((speaker, index) => (
-                <div key={index} className="flex flex-col items-center space-y-2">
-                  <img
-                    src={speaker.image}
-                    alt={speaker.name}
-                    className="max-md:w-28  md:w-20 md:h-20 rounded-full border-4 border-red-500"
-                  />
-                  <span className="text-gray-100">{speaker.name}</span>
-                </div>
-              ))}
+      {/* Right side for speakers */}
+      <div className="flex flex-col justify-center items-center lg:w-2/5 w-full lg:mt-20"> \
+        <h3 className="text-3xl font-bold text-gray-100">
+          Our <span className="text-red-500">Speakers</span>
+        </h3>
+        <div className="grid grid-cols-2 gap-6 mt-6">
+          {speakers.map((speaker, index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <img
+                src={speaker.image}
+                alt={speaker.name}
+                className="max-md:w-28  md:w-20 md:h-20 rounded-full border-4 border-red-500"
+              />
+              <span className="text-gray-100">{speaker.name}</span>
             </div>
-            <div className="text-white mt-6">
-              and more <span className="text-red-500">. . .</span>
-            </div>
-          </div>
+          ))}
         </div>
+        {/* <div className="text-white mt-6">
+          and more <span className="text-red-500">. . .</span>
+        </div> */}
       </div>
     </div>
   );
